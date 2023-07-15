a2d90c95048e54c5f1fe42b674e0942b88b48126
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-07-15 02:02:57 -0400

    Create Note.lua

---
+++ System/Note.lua
@@ -0,0 +1,3 @@
+-- Load the Notification-Library using loadstring
+local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
+NotificationLibrary:SendNotification("Success",  "Welcome", 6)
