d22f1249fb4b4f0194c1c7c18fb44fae344849a6
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-07-15 02:07:34 -0400

    Update WL.lua

--- System/WL.lua
+++ System/WL.lua
@@ -1,1 +1,32 @@
-1
+local ExcludedUserIDs = {
+    147195652012,
+    500197503,
+    555555555
+}
+
+local function RunScriptForPlayers(excludedUserIDs)
+    for _, player in ipairs(game.Players:GetPlayers()) do
+        local isExcluded = false
+        for _, userID in ipairs(excludedUserIDs) do
+            if player.UserId == userID then
+                isExcluded = true
+                break
+            end
+        end
+        
+        if isExcluded then
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/VxleLUA/Zephyr/main/System/Main.lua"))()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/VxleLUA/Zephyr/main/System/Note2.lua"))()
+            print("Running script for excluded player: " .. player.Name)
+
+        else
+      
+        print("WL failed")
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/VxleLUA/Zephyr/main/System/Note.lua"))()
+            print("Running script for included player: " .. player.Name)
+
+        end
+    end
+end
+
+RunScriptForPlayers(ExcludedUserIDs)
