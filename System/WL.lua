0f3f3826e4bfb9f9ee5b7ddeb511feb11333139c
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-07-15 02:14:37 -0400

    Update WL.lua

--- System/WL.lua
+++ System/WL.lua
@@ -4,33 +4,26 @@ local ExcludedUserIDs = {
     555555555
 }
 
-local function RunScriptForPlayers(excludedUserIDs)
-    local localPlayer = game.Players.LocalPlayer
+local function RunScriptForPlayer(excludedUserIDs)
+    local player = game.Players.LocalPlayer -- Get the local player
+    local isExcluded = false
 
-    for _, player in ipairs(game.Players:GetPlayers()) do
-        if player == localPlayer then
-            -- Skip the local player
-            continue
+    for _, userID in ipairs(excludedUserIDs) do
+        if player.UserId == userID then
+            isExcluded = true
+            break
         end
+    end
 
-        local isExcluded = false
-        for _, userID in ipairs(excludedUserIDs) do
-            if player.UserId == userID then
-                isExcluded = true
-                break
-            end
-        end
-        
-        if isExcluded then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/VxleLUA/Zephyr/main/System/Main.lua"))()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/VxleLUA/Zephyr/main/System/Note2.lua"))()
-            print("Running script for excluded player: " .. player.Name)
-        else
-            print("WL failed")
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/VxleLUA/Zephyr/main/System/Note.lua"))()
-            print("Running script for included player: " .. player.Name)
-        end
+    if isExcluded then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/VxleLUA/Zephyr/main/System/Main.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/VxleLUA/Zephyr/main/System/Note2.lua"))()
+        print("Running script for excluded player: " .. player.Name)
+    else
+        print("WL failed")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/VxleLUA/Zephyr/main/System/Note.lua"))()
+        print("Running script for included player: " .. player.Name)
     end
 end
 
-RunScriptForPlayers(ExcludedUserIDs)
+RunScriptForPlayer(ExcludedUserIDs)
