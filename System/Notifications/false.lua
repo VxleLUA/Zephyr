af60b6b16f03d2ffa0ba47b9e7dadc2ed67372d0
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-07-16 22:22:49 -0400

    Create false.lua

---
+++ System/Notifications/false.lua
@@ -0,0 +1,2 @@
+local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
+NotificationLibrary:SendNotification("Error",  "WL Failed.", 6)
