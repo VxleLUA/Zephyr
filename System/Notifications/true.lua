928e75ba5c9023f42278580c7ace34728c142676
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-07-16 22:23:27 -0400

    Create true.lua

---
+++ System/Notifications/true.lua
@@ -0,0 +1,2 @@
+local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
+NotificationLibrary:SendNotification("Success",  "Welcome", 6)
