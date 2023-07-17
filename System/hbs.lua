8701183c52aeb8fa19f2194c3c5e8303ee70de0f
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-07-16 20:53:21 -0400

    Create hbs.lua

---
+++ System/hbs.lua
@@ -0,0 +1,24 @@
+local headSize = 11
+local color = Color3.new(1, 1, 1)
+local player = game.Players.LocalPlayer
+
+local function updateRange(value)
+    local rangeValue = tonumber(value)
+    if rangeValue then
+        headSize = rangeValue
+        while wait() do
+            for _, v in ipairs(game.Players:GetPlayers()) do
+                if v ~= player and v.Character and v.Character:FindFirstChild("HumanoidRootPart") then
+                    local part = v.Character.HumanoidRootPart
+                    part.Size = Vector3.new(rangeValue, rangeValue, rangeValue)
+                    part.Transparency = 1
+                    part.BrickColor = BrickColor.new(color)
+                    part.Material = Enum.Material.ForceField
+                    part.CanCollide = false
+                end
+            end
+        end
+    end
+end
+
+updateRange(20)
