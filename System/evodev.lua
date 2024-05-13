f597577519ff8013c8ff31b68a2368fa03c79b11
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2024-05-12 22:19:11 -0400

    Update evodev.lua

--- System/evodev.lua
+++ System/evodev.lua
@@ -1,1 +1,26 @@
-
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+    Name = "Evo",
+    LoadingTitle = "Evo Developer",
+    LoadingSubtitle = "by VxleLUA",
+    ConfigurationSaving = {
+       Enabled = false,
+       FolderName = nil,
+       FileName = "Evo Save"
+    },
+    Discord = {
+       Enabled = false,
+       Invite = "noinvitelink",
+       RememberJoins = true
+    },
+    KeySystem = false,
+    KeySettings = {
+       Title = "Untitled",
+       Subtitle = "Key System",
+       Note = "No method of obtaining the key is provided",
+       FileName = "...",
+       SaveKey = true,
+       GrabKeyFromSite = true,
+       Key = "https://raw.githubusercontent.com/VxleLUA/Evo/main/MainScript/Data/Key.lua"
+    }
+})
